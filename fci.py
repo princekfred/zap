06c134c7408a704019daf82abbb4fabdf0ad9f9c
Prince Frederick Kwao <157415235+princekfred@users.noreply.github.com>
2026-03-14 14:36:44 -0500

    save

--- fci.py
+++ fci.py
@@ -2,13 +2,13 @@ from pathlib import Path
 import numpy as np
 from pyscf import gto, scf, mcscf
 
-out_path = Path("outputs/H4_trotterized/CASCI_output.txt")
+out_path = Path("outputs/CH+ Trotterized/CASCI_output.txt")
 # Molecule/setup
 mol = gto.M(
-    atom="H 0 0 0; H 0 0 3; H 0 0 6; H 0 0 9",
-    basis="sto-3g",
+    atom="C 0 0 0; H 0 0 2.261840985",
+    basis="6-31g",
     unit="angstrom",
-    charge=0,
+    charge=1,
     spin=0,
     verbose=0,
 )
@@ -18,11 +18,11 @@ mf = scf.RHF(mol)
 mf.kernel()
 
 # CASCI active space (same as your HF_0.793 workflow)
-ncas = 4
-nelecas = 4
+ncas = 6
+nelecas = 6
 
 # Number of excited states to save
-n_excited = 26
+n_excited = 117
 
 # CASCI roots: ground + excited
 mc = mcscf.CASCI(mf, ncas, nelecas)
